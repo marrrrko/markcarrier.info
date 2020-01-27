f79da7775acf4b2ed606b138a8dd034ce8c7a413
Mark Carrier <markcarrier@gmail.com>
2020-01-27 19:04:41 +0300

    Adds aws fargate task definition

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker build -t markcarrier.info .
+OWNER="markcarrier"
+REPOSITORY="markcarrier.info"
+IMAGE_NAME="markcarrier.info"
+docker build -t docker.pkg.github.com/$OWNER/$REPOSITORY/$IMAGE_NAME .
+
+#docker login -u $OWNER -p $TOKEN docker.pkg.github.com
+#docker push docker.pkg.github.com/$OWNER/$REPOSITORY/$IMAGE_NAME
+
--- run-docker-image.sh
+++ run-docker-image.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 9999:8888 markcarrier.info
+OWNER="markcarrier"
+REPOSITORY="markcarrier.info"
+IMAGE_NAME="markcarrier.info"
+docker run -d -p 9999:8888 docker.pkg.github.com/$OWNER/$REPOSITORY/$IMAGE_NAME
