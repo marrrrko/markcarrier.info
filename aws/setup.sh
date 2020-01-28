1cca4f80600be7980d3be0139fdd65f11726ef33
Mark Carrier <markcarrier@gmail.com>
2020-01-28 18:56:51 +0300

    Improves AWS command script for fargate setup

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
-# Create an ECR repository
-aws ecr create-repository --repository-name markcarrier-resume--region ca-central-1
+source ./set_aws_config.sh
 
-# Register the task definition
-aws ecs register-task-definition --region ca-central-1 --cli-input-json file://./resume-aws-fargate-task-definition.json
-
-# Create an ECS Fargate Cluster
-aws ecs create-cluster --region ca-central-1 --cluster-name markcarrier-resume
-
-# Create the ECS Service
-aws ecs create-service --region ca-central-1 --cluster markcarrier-resume --service-name resume-service --task-definition markcarrier-resume:1 --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0cccd80fd47d1d310,subnet-07acff8b9d97d4687],securityGroups=[sg-027d5ab304d610169]}"
+if [ "$1" == "create-repo" ]
+then
+    # Create an ECR repository
+    aws ecr create-repository --repository-name $CONTAINER_REPO_NAME --region $REGION
+elif [ "$1" == "register-task" ]
+then
+    # Register the task definition
+    aws ecs register-task-definition --region $REGION --cli-input-json $TASK_DEF_FILE
+elif [ "$1" == "create-cluster" ]
+then
+    # Create an ECS Fargate Cluster
+    aws ecs create-cluster --region $REGION --cluster-name $CLUSTER_NAME
+elif [ "$1" == "create-service" ]
+then
+    # Create the ECS Service
+    aws ecs create-service \
+     --region $REGION \
+     --cluster $CLUSTER_NAME \
+     --service-name $SERVICE_NAME \
+     --task-definition $TASK_DEF_NAME \
+     --desired-count 1 \
+     --launch-type "FARGATE" \
+     --load-balancers $LOAD_BALANCER_CONFIG \
+     --network-configuration $NETWORK_CONFIG
+elif [ "$1" == "force-redeploy" ]
+then
+    # Force service deploy
+    aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --force-new-deployment
+else
+    echo "You must call this script with one of the following commands:"
+    echo "  create-repo"
+    echo "  register-task"
+    echo "  create-cluster"
+    echo "  create-service"
+    echo "  force-redeploy"
+    echo ""
+    echo "You must also set the following environment variables in a script called set_aws_config.sh:"    
+    echo '  CONTAINER_REPO_NAME="XYZ"'
+    echo '  REGION="XYZ"'
+    echo '  TASK_DEF_FILE="file://./resume-aws-fargate-task-definition.json"'
+    echo '  TASK_DEF_NAME="XYZ:1"'
+    echo '  CLUSTER_NAME="XYZ"'
+    echo '  SERVICE_NAME="XYZ"'
+    echo '  LOAD_BALANCER_CONFIG="targetGroupArn=arn:aws:elasticloadbalancing:...,containerName=XYZ,containerPort=1234" \'
+    echo '  NETWORK_CONFIG="awsvpcConfiguration={subnets=[subnet-abc123,subnet-abc456],securityGroups=[sg-abc123],assignPublicIp=ENABLED}"'
+fi
