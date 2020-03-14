c49eda29d9e9ee542bc4a9b9fb94cdd9d8b56e3e
Mark Carrier <markcarrier@gmail.com>
2020-03-14 23:25:25 +0300

    Improves blog styling and publishing

--- servers/blog.js
+++ servers/blog.js
@@ -1,22 +1,30 @@
 const mount = require('koa-mount')
 const serve = require('koa-static')
-const send = require('koa-send')
+const moment = require('moment')
 const Koa = require('koa')
 const _ = require('lodash')
 const fs = require('fs-extra')
 const frontMatter = require('front-matter')
 const marked = require('marked')
+const renderer = new marked.Renderer();
+renderer.link = function(href, title, text) {
+    const link = marked.Renderer.prototype.link.apply(this, arguments);
+    return link.replace("<a","<a target='_blank'");
+}
+marked.setOptions({ renderer: renderer })
 const mustache = require('mustache');
 const sass = require('node-sass');
 
 const srcDir = __dirname + "/../writings-src"
 const remote = "https://github.com/MarkCarrier/public-writings.git"
 const sassSrc = "./mini-mark.scss"
+let postData = null
 
 module.exports = async function startBlogServer(port) {
 
     const css = await buildCSS()
-    const posts = await loadPosts()
+    postData = await loadPostData()
+    setInterval(reloadPosts, 15 * 1000 * 60)
 
     return new Promise(function(resolve, reject) {
         try {
@@ -32,10 +40,18 @@ module.exports = async function startBlogServer(port) {
             })
             app.use(async (ctx, next) => {
                 const posturl = ctx.path.replace("/post/","").toUpperCase()
-                const matchedPost = posts[posturl]
-                if(ctx.path.startsWith("/post/") && matchedPost) {                    
+                
+                let matchedPost = postData.postsById[posturl]
+                const matchedUrlPostId = postData.postIdsByUrl[posturl]
+                if(!matchedPost && matchedUrlPostId) {
+                    matchedPost = postData.postsById[matchedUrlPostId]
+                    if(!matchedPost.indexed) //Only indexed posts can be accessed by url
+                        matchedPost = null
+                }            
+                
+                if(ctx.path.startsWith("/post/") && matchedPost && matchedPost.published) {                    
                     ctx.type = 'text/html';              
-                    ctx.body = mustache.render(postTemplate, { title: matchedPost.attributes.title, html: matchedPost.html})          
+                    ctx.body = matchedPost.html          
                 } else {
                     ctx.status = 404
                     ctx.body = "Post not found"
@@ -49,26 +65,45 @@ module.exports = async function startBlogServer(port) {
     })
 }
 
-async function loadPosts() {
+function reloadPosts() {
+    loadPostData()
+    .then(function(updatedPosts) {
+        postData = updatedPosts
+    })
+}
+
+async function loadPostData() {
     const postFiles = await loadPostsFromSource()
     const posts = postFiles.map(postfile => frontMatter(postfile))
+    const postsById = posts.reduce(
+        (dict, post) => ({
+            ...dict,
+            [post.attributes.id.toUpperCase()]: { 
+                ...post.attributes,
+                html: mustache.render(postTemplate, { 
+                    ...post.attributes,
+                    dateString: moment(post.attributes.date).format("MMMM Do YYYY"),
+                    html: marked(post.body)})
+            }}),
+        {})
 
-    const urlPosts = posts.map(post => {
-        return post.attributes.urls.map(url => {
-            return {
-                url: url.toUpperCase(),
-                ...post,
-                html: marked(post.body)
-            }
-        })    
-    })
+    const urlGroups = Object.values(postsById)
+        .map(p => p.urls.map(u => ({ 
+            url: u.toUpperCase(),
+            postId: p.id.toUpperCase()
+        })))
+    const urls = _.flatten(urlGroups)
+    const postIdsByUrl = urls.reduce(
+        (dict, urlIdMapping) => ({
+            ...dict,
+            [urlIdMapping.url]: urlIdMapping.postId
+        }),
+        {})
 
-    return _.flatten(urlPosts)
-    .reduce((dict, post) => {
-        dict[post.url] = post
-        return dict
-    }, {})
-
+    return {
+        postsById,
+        postIdsByUrl
+    }
 }
 
 async function buildCSS() {
@@ -113,22 +148,35 @@ async function loadSrcFiles() {
 
 const postTemplate = `
 <html>
-<head>
-    <title>{{title}}</title>
-    <link href="https://fonts.googleapis.com/css?family=Crimson+Text&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
-</head>    
-    <br /><br />
-    <div class="container">    
-        <div class="row">
-            <div class="col-sm-0 col-md-1 col-lg-2"></div>
-            <div class="col-sm-12 col-md-10 col-lg-8">{{{html}}}</div>
-            <div class="col-sm-0 col-md-1 col-lg-2"></div>
+    <head>
+        <title>{{title}}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Spartan|Crimson+Text&display=swap" rel="stylesheet">    
+        <link rel="stylesheet" href="/style.css">
+        {{^indexed}}
+        <meta name="robots" content="noindex">
+        {{/indexed}}
+    </head>
+    <body>
+        <div class="page-line"></div>
+        <div class="container">            
+            <div class="row"><br /></div>
+            <div class="row">
+                <div class="col-sm-0 col-md-2 col-lg-3"></div>
+                <div class="col-sm-12 col-md-8 col-lg-6">
+                    <div class="side-note">
+                        <div>{{dateString}}</div><br />
+                    </div>
+                    <div class="side-note">
+                        <div>by {{author}}</div>
+                    </div>
+                    <h1>{{title}}</h1>
+                    {{{html}}}
+                </div>
+                <div class="col-sm-0 col-md-2 col-lg-3"></div>
+            </div>
         </div>
-    </div>
-    <br /><br /><br />
+        <br /><br /><br />
+    </body>
 </html>
 `
