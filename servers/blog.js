b8d837da609396ae9fdc672330bd5f21f21a6052
Mark Carrier <markcarrier@gmail.com>
2020-03-17 23:07:32 +0300

    Fixes top margin inconsistencies with FF

--- servers/blog.js
+++ servers/blog.js
@@ -185,7 +185,7 @@ const postTemplate = `
     <body>
         <!--<div class="page-line"></div>-->
         <div class="container">            
-            <div class="row"><br /></div>
+            <div class="row"></div>
             <div class="row">
                 <div class="col-sm-0 col-md-2 col-lg-3"></div>
                 <div class="col-sm-12 col-md-8 col-lg-6">
